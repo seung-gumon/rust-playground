b0b2a158013d7eabe434c3307b999818e24d9d61
seungseok <seungseok@argoskyc.com>
2024-11-19 01:00:29 +0900

    loop-1

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,24 @@
-enum Number{
-      U32(u32),
-      I32(i32)
-}
-
-fn get_number(input : i32) -> Number {
-      match input.is_positive() {
-            true => Number::U32(input as u32),
-            false => Number::I32(input)
-      }
-}
-
-
 fn main() {
-      let my_vec = vec![get_number(-800) , get_number(8)];
 
-      for item in my_vec {
-            match item {
-                  Number::U32(num) => println!("U32: {}", num),
-                  Number::I32(num) => println!("I32: {}", num),
+      let mut counter = 0;
+      let mut counter2 = 0;
+
+
+      'first_loop:loop {
+            println!("First loop: {}", counter);
+            counter += 1;
+
+            if counter == 10 {
+                  'second_loop:loop {
+                        println!("Second loop: {}", counter2);
+                        counter2 += 1;
+
+                        if counter2 == 10 {
+                              break 'first_loop;
+                        }
+                  }
             }
       }
+
+
 }
