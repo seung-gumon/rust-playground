46b380cae003e59a8f73e30a688d19f45d01b221
seungseok <seungseok@argoskyc.com>
2024-11-19 00:50:27 +0900

    enum-4

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
-enum Star {
-      BrownDwarf = 10,
-      RedDwarf = 50,
-      YellowStar = 100,
-      RedGiant = 1000,
-      DeadStar
+enum Number{
+      U32(u32),
+      I32(i32)
+}
+
+fn get_number(input : i32) -> Number {
+      match input.is_positive() {
+            true => Number::U32(input as u32),
+            false => Number::I32(input)
+      }
 }
 
 
 fn main() {
-      use Star::*;
-      let starvec = vec![BrownDwarf, RedDwarf, YellowStar, RedGiant, DeadStar];
+      let my_vec = vec![get_number(-800) , get_number(8)];
 
-      for star in starvec {
-            match star as u32 {
-                  size if size <= 80 => println!("Not the biggest star : {}" , size),
-                  size if size >= 80 => println!("Pretty big star : {}", size),
-                  _ => println!("Some other star")
+      for item in my_vec {
+            match item {
+                  Number::U32(num) => println!("U32: {}", num),
+                  Number::I32(num) => println!("I32: {}", num),
             }
       }
-
-      println!("The biggest star is : {}", DeadStar as u32);
 }
